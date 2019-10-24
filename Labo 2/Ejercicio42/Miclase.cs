5aa9833bd215c248c1bf9e2175d905ba4b22b6a4
Massimo Di Berardino <maxdo1996@gmail.com>
2019-10-24 17:07:30 -0300

    excepciones

---
+++ Labo 2/Ejercicio42/Miclase.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio42
+{
+    public class Miclase
+    {
+
+        int b;
+
+        public Miclase(string msg)
+        {
+
+        }
+        public Miclase ()
+        {
+            try
+            {
+                new Miclase(5);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                throw e;
+            }
+        }
+
+        public Miclase(int a)
+        {
+            this.b = a;
+            try
+            {
+                Miclase.MiMetodo();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                throw e;
+            }
+        }
+
+        static void MiMetodo ()
+        {
+            throw new DivideByZeroException();
+        }
+
+        public void MiMetodo2 ()
+        {
+            try
+            {
+                new Miclase();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                throw new UnaExcepcion($"Mensaje de exception", e);
+            }
+        }
+    }
+
+    public class UnaExcepcion : Exception
+    {
+        string mensaje;
+
+        public UnaExcepcion(string mensaje, Exception e) : base(mensaje, e)
+        {
+            this.mensaje = mensaje;
+        }
+
+        public UnaExcepcion(string mensaje) : base(mensaje)
+        {
+            this.mensaje = mensaje;
+        }
+    }
+}
---
+++ Labo 2/Ejercicio42/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio42
+{
+    class Program
+    {
+
+        
+        static void Main(string[] args)
+        {
+
+            Miclase mc = new Miclase("hola");
+
+            try
+            {
+                mc.MiMetodo2();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"main {e.Message}");
+                Console.ReadKey();
+            }
+        }
+    }
+}
